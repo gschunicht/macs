bf30781e31dc8fc126d0774996de295fd5d0cec3
MattFolsom <40132734+MattFolsom@users.noreply.github.com>
2018-06-28 10:38:17 -0500

    Delete db_con.php
    Not used in Telerik based UI

--- website/db_con.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once('sql_password.php');
-$db = new PDO('mysql:dbname=macs;host=127.0.0.1;charset=utf8', 'root', $sql_pw);
-
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
