b21caf63013e24cac76183c49b5595b42000c43c
MattFolsom <40132734+MattFolsom@users.noreply.github.com>
2018-06-28 10:34:02 -0500

    Delete module_setup_helper_reader.php
    Not used in Telerik based UI

--- website/php_module/module_setup_helper_reader.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file="/tmp/macs_log.txt";
-$myfile = fopen($file, "r") or die("Unable to open file!");
-$data=fread($myfile,filesize($file));
-echo str_replace("\r\n","<br>",$data);
-fclose($myfile);
-?>
