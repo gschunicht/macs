11d8ffac832ab1f15f0c237a5521f704ddae3658
Geoff Schunicht <schunicht@gmail.com>
2023-06-24 12:53:10 -0500

    missing files

---
+++ firmware/main.h
@@ -0,0 +1,18 @@
+#define DEBUG_JKW_MAIN 0
+
+// used for wifi connected or not an a 200 from the web service
+#define NOT_CONNECTED  0
+#define CONNECTED  1
+
+void set_connected(int status);
+void set_wifi_connected(int status, bool force);
+bool read_EEPROM();
+void goto_update_mode();
+bool update_ids(bool force);
+uint32_t set_relay_state(int8_t input);
+bool validate_tag(uint8_t *buf,uint32_t *tag);
+
+void create_report(uint8_t event,uint32_t badge,uint32_t extrainfo);
+bool tag_found(uint8_t *buf,uint32_t *tag);
+bool access_test(uint32_t tag);
+bool check_db_for_one_tag(int currentTag);
